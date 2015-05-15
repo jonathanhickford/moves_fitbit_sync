d17186739582acf64102a4d6296c2994a6bd26d8
Jonathan Hickford <jonathan.hickford@gmail.com>
2015-05-15 15:40:27 +0100

    started fitbit auth

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 gem "moves"
+gem 'fitgem'
 gem "omniauth"
 gem 'omniauth-moves', github: 'nickelser/omniauth-moves'
+gem 'omniauth-fitbit', github: 'tkgospodinov/omniauth-fitbit'
 gem "sinatra"
--- moves_app.rb
+++ moves_app.rb
@@ -6,19 +6,21 @@ require 'omniauth'
 
 require 'omniauth'
 require 'omniauth-moves'
+require 'omniauth-fitbit'
 require 'moves'
+require 'fitgem'
 require 'json'
 
 
 CLIENT_SECRETS_FILE = 'client_secrets.json'
 MOVES_AUTH_FILE = 'moves_auth.json'
-
+FITBIT_AUTH_FILE = 'fitbit_auth.json'
 
 
 class MovesApp < Sinatra::Base
   
   helpers do
-    def load_access_token_from_file
+    def load_moves_access_token_from_file
       begin
         if File.file?(MOVES_AUTH_FILE)
           auth = JSON.parse(File.read(MOVES_AUTH_FILE))
@@ -44,15 +46,16 @@ class MovesApp < Sinatra::Base
   use OmniAuth::Builder do
     client_secrets = JSON.parse(File.read(CLIENT_SECRETS_FILE))
     provider :moves, client_secrets['moves_client_id'], client_secrets['moves_client_secret']
+    provider :fitbit, client_secrets['fitbit_client_key'], client_secrets['fitbit_client_secret']
   end
 
 
   get '/' do
-    session['access_token'] = load_access_token_from_file()
+    session['moves_access_token'] = load_moves_access_token_from_file()
     erb "
-      <p><%= session['access_token'] %></p>
-      <p><a href='/grant_access'>Grant Access to Moves Account</a></p>
-      <p><a href='/summary'>Todays activities</a></p>
+      <p><%= session['moves_access_token'] %></p>
+      <p><a href='/grant_access'>Grant Access to Accounts</a></p>
+      <p><a href='/moves_summary'>Todays activities</a></p>
     "
   end
 
@@ -62,13 +65,21 @@ class MovesApp < Sinatra::Base
      <form action='/auth/moves' method='post'>
         <input type='submit' value='Sign in with Moves'/>
       </form>
+    <form action='/auth/fitbit' method='post'>
+        <input type='submit' value='Sign in with Fitbit'/>
+      </form>
     "
   end
 
   get '/auth/:provider/callback' do
     json = JSON.pretty_generate(request.env['omniauth.auth'])
-    File.open(MOVES_AUTH_FILE, 'w') { |file| file.write(json) }
-    session['access_token'] = load_access_token_from_file()
+    
+    if params[:provider] == "moves"
+      File.open(MOVES_AUTH_FILE, 'w') { |file| file.write(json) }
+      session['moves_access_token'] = load_moves_access_token_from_file()
+    elsif params[:provider] == "fitbit"
+      File.open(FITBIT_AUTH_FILE, 'w') { |file| file.write(json) }
+    end
 
     erb "<h1>#{params[:provider]}</h1>
          <pre>#{json}</pre>"
@@ -78,11 +89,23 @@ class MovesApp < Sinatra::Base
     erb "<h1>Authentication Failed:</h1><h3>message:<h3><pre>#{params}</pre>"
   end
 
-  get '/summary' do
-    moves = Moves::Client.new(session['access_token'])
+  get '/moves_summary' do
+    moves = Moves::Client.new(session['moves_access_token'])
     
     erb "<h1>Summary:</h1><pre>#{JSON.pretty_generate(moves.daily_activities)}</pre>"
   end
   
+  get '/fitbit_summary' do
+    client = Fitgem::Client.new(config[:oauth])
+    
+    erb "<h1>Summary:</h1><pre>#{JSON.pretty_generate(moves.daily_activities)}</pre>"
+  end
+
+
+
+
+
+client = Fitgem::Client.new(config[:oauth])
+
 end
 
