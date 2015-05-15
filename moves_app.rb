cfcd939c7cc0cd93d946ec9e6621405bd02ecd73
Jonathan Hickford <jonathan.hickford@gmail.com>
2015-05-15 11:37:12 +0100

    updated tobe ready for multiple auth

--- moves_app.rb
+++ moves_app.rb
@@ -10,34 +10,47 @@ require 'moves'
 require 'json'
 
 
-MOVES_CLIENT_SECRETS_FILE = 'moves_client_secrets.json'
+CLIENT_SECRETS_FILE = 'client_secrets.json'
 MOVES_AUTH_FILE = 'moves_auth.json'
 
 
 
 class MovesApp < Sinatra::Base
+  
+  helpers do
+    def load_access_token_from_file
+      begin
+        if File.file?(MOVES_AUTH_FILE)
+          auth = JSON.parse(File.read(MOVES_AUTH_FILE))
+          auth['credentials']['token']
+        else
+          nil
+        end
+      rescue
+        nil
+      end
+    end
+  end
+
+
+
+
   configure do
     set :sessions, true
     set :inline_templates, true
-    set :access_token, nil
-    if File.file?('auth.json')
-      auth = JSON.parse(File.read(MOVES_AUTH_FILE))
-      p auth['credentials']['token']
-      set :access_token, auth['credentials']['token']
-    end
-
-
+    
   end
 
   use OmniAuth::Builder do
-    moves_client_secrets = JSON.parse(File.read(MOVES_CLIENT_SECRETS_FILE))
-    provider :moves, moves_client_secrets['client_id'], client_secrets['client_secret']
+    client_secrets = JSON.parse(File.read(CLIENT_SECRETS_FILE))
+    provider :moves, client_secrets['moves_client_id'], client_secrets['moves_client_secret']
   end
 
 
   get '/' do
+    session['access_token'] = load_access_token_from_file()
     erb "
-      <p><%= settings.access_token %></p>
+      <p><%= session['access_token'] %></p>
       <p><a href='/grant_access'>Grant Access to Moves Account</a></p>
       <p><a href='/summary'>Todays activities</a></p>
     "
@@ -55,8 +68,7 @@ class MovesApp < Sinatra::Base
   get '/auth/:provider/callback' do
     json = JSON.pretty_generate(request.env['omniauth.auth'])
     File.open(MOVES_AUTH_FILE, 'w') { |file| file.write(json) }
-    auth = JSON.parse(File.read(MOVES_AUTH_FILE))
-    set :access_token, auth['credentials']['token']
+    session['access_token'] = load_access_token_from_file()
 
     erb "<h1>#{params[:provider]}</h1>
          <pre>#{json}</pre>"
@@ -67,7 +79,7 @@ class MovesApp < Sinatra::Base
   end
 
   get '/summary' do
-    moves = Moves::Client.new(settings.access_token)
+    moves = Moves::Client.new(session['access_token'])
     
     erb "<h1>Summary:</h1><pre>#{JSON.pretty_generate(moves.daily_activities)}</pre>"
   end
