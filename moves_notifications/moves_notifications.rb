3c63377f10428e11c5aa3a508ba672f231a79230
Jonathan Hickford <jonathan.hickford@gmail.com>
2015-07-02 20:06:52 +0100

    dumping body to string to debug and test signatures

--- moves_notifications/moves_notifications.rb
+++ moves_notifications/moves_notifications.rb
@@ -19,7 +19,7 @@ class MovesNotifications < Sinatra::Base
   	s = env['HTTP_X_MOVES_SIGNATURE']
   	t = env['HTTP_X_MOVES_TIMESTAMP']
   	n = env['HTTP_X_MOVES_NONCE']
-  	b = request.body
+  	b = request.body.read.to_s
 
   	message = [b,t,n].join('|')
   	calculated_sig = Base64.encode64("#{OpenSSL::HMAC.digest('sha1',ENV['MOVES_CLIENT_SECRET'], message)}").chomp
