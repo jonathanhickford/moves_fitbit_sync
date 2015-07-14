0c564ab749885df017c86e708661e8b669d623d2
Jonathan Hickford <jonathan.hickford@gmail.com>
2015-07-14 08:05:27 +0100

    Updated rake file to exclude test tasks outside of test env

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,10 @@
-require 'ci/reporter/rake/rspec'
-require 'rspec/core/rake_task'
 require 'resque/tasks'
-RSpec::Core::RakeTask.new(:spec => ["ci:setup:rspec"])
-task :default => [:spec]
+
+if ENV['RACK_ENV'] == 'test'
+	require 'ci/reporter/rake/rspec'
+	require 'rspec/core/rake_task'
+	RSpec::Core::RakeTask.new(:spec => ["ci:setup:rspec"])
+	task :default => [:spec]
+else
+	task :default => ['resque:work']
+end
